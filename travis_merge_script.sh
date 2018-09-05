87fd3cd70dd45f56a7c8dc485ecd2eca144fe7e3
Full Bright <full3right@gmail.com>
2018-09-05 20:23:34 +0000

    On success, push with the github tocken

--- travis_merge_script.sh
+++ travis_merge_script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" != "staged" ]; then
+    echo "This is not the staged branch. No merge."
     exit 0;
 fi
 
-
+echo "We are on the staged branch. Merging to master."
+echo "Travis commit: $TRAVIS_COMMIT. Travis repo slug: $TRAVIS_REPO_SLUG"
 git config --global user.email 'full3right@gmail.com'
 git config --global user.name 'Full Bright'
 git remote set-branches --add origin master
@@ -12,4 +14,4 @@ git fetch
 git reset --hard
 git checkout master
 git merge --ff-only "$TRAVIS_COMMIT"
-git push git+ssh://git@github.com/${TRAVIS_REPO_SLUG}.git master
+git push https://fullbright:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master
