ea76630a063a9a1673075179052706b3a9608445
Full Bright <full3right@gmail.com>
2018-11-16 22:00:04 +0100

    Add electron compilation scripts

--- src/pages/post-detail/post-detail.ts
+++ src/pages/post-detail/post-detail.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-@IonicPage()
+//@IonicPage()
 @Component({
   selector: 'page-post-detail',
   templateUrl: 'post-detail.html',
